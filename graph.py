75092c7bf43126d88a68b702bbf7382863021de1
yuvallinux <yuvddd05@gmail.com>
2024-07-06 18:28:46 +0300

    create graph object

--- graph.py
+++ graph.py
@@ -2,7 +2,7 @@ class Node:
     nodes = 1
     
 
-    def __init__(self, value, *neighbors) -> None:
+    def __init__(self, value, neighbors) -> None:
         self.__value = value
         self.__id = Node.nodes
         Node.nodes += 1        
@@ -34,13 +34,21 @@ class Node:
         return self.__neighbors
 
 
-    def add_neighbor(self, *new_neighbors):
-        self.__neighbors.extend(list(new_neighbors))
+    def add_neighbor(self, new_neighbors):
+        
+        if type(new_neighbors) != list:
+            new_neighbors = [new_neighbors]
+
+        self.__neighbors.extend(new_neighbors)
 
 
-    def remove_neighbor(self, *neighbors_remove):
+    def remove_neighbor(self, neighbors_remove):
+
+        if type(new_neighbors) != list:
+            new_neighbors = [new_neighbors]
         
         for neighbor in neighbors_remove:
+        
             if neighbor in self.__neighbors:
                 self.__neighbors.remove(neighbor)
 
@@ -51,26 +59,44 @@ class Node:
 
 class Graph:
 
-
     def __init__(self) -> None:
         self.nodes = {}
 
 
+    def create_node(self, value, *neighbors):
+        node = Node(value, neighbors)
+        self.add_node(node)
+
+
     def add_node(self, node):
-        pass
+        
+        if node.id not in self.nodes:
+            self.nodes[node.id] = node
+                    
+        for neighbor in node.neighbors:
+            self.nodes[neighbor].add_neighbor(node.id)
+
+
+    def remove_node(self, node_id):
+        node = self.nodes[node_id]
+
+        if node.id in self.nodes:
+            del self.nodes[node]
+        
+        for neighbor in node.neighbors:
+            self.nodes[neighbor.id].remove_neighbor(node.id)
 
 
 def main():
-    n = Node(5)
-    n1 = Node(5, 1)
-    n2 = Node(5, 1, 2)
+    g = Graph()
 
-    n.add_neighbor(2, 3)
-    n1.add_neighbor(3)
+    g.create_node(5)
+    g.create_node(5, 1)
+    g.create_node(5, 1, 2)
 
-    print(n)
-    print(n1)
-    print(n2)
+    print(g.nodes[1])
+    print(g.nodes[2])
+    print(g.nodes[3])
 
 
 if __name__ == "__main__":
